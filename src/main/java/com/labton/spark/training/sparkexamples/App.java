c5ceb005d1ff690d0a4481c9b6360018a2b7aed5
Eduardo Carneiro <Eduardo Carneiro@10.4.4.155>
2019-02-19 19:47:22 -0500

    Commit to Gihub

---
+++ src/main/java/com/labton/spark/training/sparkexamples/App.java
@@ -0,0 +1,14 @@
+package com.labton.spark.training.sparkexamples;
+
+/**
+ * Hello world!
+ *
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
